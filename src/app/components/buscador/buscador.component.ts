6acdde2a09344db75f74005fd1ff88ac98663d55
rogerf0212 <rogerdt05@gmail.com>
2022-07-22 18:00:54 -0400

    4 files modified

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-buscador',
@@ -6,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscadorComponent implements OnInit {
 
-  constructor() { }
+  heroes:any[] = [];
+  termino:string;
+
+  constructor( private activatedRoute:ActivatedRoute,
+                private _heroesService: HeroesService) { 
+
+  }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes(params['termino']);
+    })
+    
+
   }
 
 }
