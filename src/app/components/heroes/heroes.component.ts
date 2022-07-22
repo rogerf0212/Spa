847b4c977741ded606e1328d012692bce5607342
rogerf0212 <rogerdt05@gmail.com>
2022-07-22 16:20:30 -0400

    4 files modified, 1 added

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesService } from '../../servicios/heroes.service';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
+  
+  heroes:Heroe[] = [];
 
   constructor( private _heroesService:HeroesService ) { 
-    
   }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeores();
   }
 
 }
