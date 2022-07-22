2af09c749783efe67ae573ba4f232bc0bcf2dd9c
rogerf0212 <rogerdt05@gmail.com>
2022-07-22 10:46:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-44q5fm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-44q5fm)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent,
+    NavbarComponent,
+    HomeComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
