efcbc0b0448910880175ce460550ea7f873cb9ee
rogerf0212 <rogerdt05@gmail.com>
2022-07-22 15:43:08 -0400

    6 files modified, 8 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+//rutas
+import { APP_ROUTING } from './app.routes'
+
+// servicios
+import { HeroesService } from './servicios/heroes.service';
+
+//componentes
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    APP_ROUTING ],
+  providers: [
+    HeroesService
+  ],
   declarations: [ 
     AppComponent, 
     HelloComponent,
     NavbarComponent,
+    AboutComponent,
+    HeroesComponent,
     HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
